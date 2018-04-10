02c98ac35a8f2942933114554686e38591d51007
natwit <natwit442@gmail.com>
2018-04-10 20:58:28 +0700

    new version updated

---
+++ python.py
@@ -0,0 +1,7 @@
+mymixList = ["hey", 1, ["ValueinList1", "ValueinList2", 2 ]]
+
+for element in mymixList:
+    if isinstance(element, list):
+        print (element)
+    else:
+        print("it is not a list.")
